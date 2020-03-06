45478100845002dd379e5c8ee74d832422c950ac
Guilherme Farias <guilherme.nf.gf@gmail.com>
2020-03-06 14:35:39 -0300

    Modelos BD

--- projetoSite/app/models.py
+++ projetoSite/app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    SEXO_CHOICES = (
+        ("feminino", "Feminino"),
+        ("masculino", "Masculino"),
+    )
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50,null=False)
+    email = models.EmailField(max_length=30, null=False)
+    data_nascimento = models.DateField(null=False,verbose_name="Data de Nascimento")
+    cpf = models.CharField(max_length=20, null=False)
+    sexo = models.CharField(max_length=20,null=False,choices=SEXO_CHOICES)
+    telefone = models.CharField(max_length=20,null=False)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50, null=False)
+    quantidade = models.IntegerField(max_length=100)
+    valor = models.FloatField(null=False)
+    foto_capa = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.nome
+
+class PedidoRegistro(models.Model):
+    id = models.IntegerField(primary_key=True)
+    dataPedido = models.DateField(null=False)
+    valorTotal = models.FloatField(null=False)
+    usuario = models.ForeignKey(Usuario)
+    produto = models.ManyToManyField(Produto)
+
+    def __str__(self):
+        return self.id
